03dd435b96a94538c91302c9863ba0077237f011
Keerthi Gowda <quic_kbalehal@quicinc.com>
2025-02-18 15:30:35 +0530

    securemsm-kernel: smmu_proxy: Update bus size to 32 bit
    Added CONFIG_QCOM_LEGACY_ADDRESS_BUS_SIZE for 32 bit address
    bus support.
    
    Return ENOSYS from panic handler to invoke default panic handler to print address information during S1 page fault.
    
    Change-Id: I175a5cb5ba7c885197ec5dd6d39f267afe113683
    Signed-off-by: Keerthi Gowda <quic_kbalehal@quicinc.com>

--- build/canoevm.bzl
+++ build/canoevm.bzl
@@ -12,6 +12,7 @@ def define_canoevm():
             "CONFIG_QTI_SMMU_PROXY",
             "CONFIG_QCOM_SI_CORE",
             "CONFIG_ARCH_QTI_VM",
+            "CONFIG_QCOM_LEGACY_ADDRESS_BUS_SIZE",
         ],
      )
     define_vm_modules(
@@ -25,5 +26,6 @@ def define_canoevm():
             "CONFIG_QTI_SMMU_PROXY",
             "CONFIG_QCOM_SI_CORE",
             "CONFIG_ARCH_QTI_VM",
+            "CONFIG_QCOM_LEGACY_ADDRESS_BUS_SIZE",
         ],
      )
--- smmu-proxy/qti-smmu-proxy-tvm.c
+++ smmu-proxy/qti-smmu-proxy-tvm.c
@@ -741,7 +741,7 @@ static int proxy_fault_handler(struct iommu_domain *domain, struct device *dev,
 {
 	dev_err(dev, "Context fault with IOVA %lx and fault flags %d\n", iova, flags);
 
-	return -EINVAL;
+	return -ENOSYS;
 }
 
 static int cb_probe_handler(struct device *dev)
@@ -768,7 +768,6 @@ static int cb_probe_handler(struct device *dev)
 
 	dma_set_max_seg_size(dev, DMA_BIT_MASK(32));
 
-     /* Is address bus to Camera HW of 32bits or 36bits */
 #if IS_ENABLED(CONFIG_QCOM_LEGACY_ADDRESS_BUS_SIZE)
 	ret = dma_coerce_mask_and_coherent(dev, DMA_BIT_MASK(32));
 #else
