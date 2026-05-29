CodeLinaro-mirror/la_platform_vendor_qcom_opensource_securemsm-kernel

securemsm-kernel: Add hibernate_tzdata_mgr
Add hibernate_tzdata_mgr to notify HibernateTzDataMgr TZ service to notify
for hibernate entry and event to save and restore TZ Data.

Change-Id: Icc52c91d19ac69ada9833933a50d7fb947161bff
Signed-off-by: Om Prakash Singh <omprsing@qti.qualcomm.com>

securemsm_kernel_product_board.mk
@@ -114,6 +114,11 @@ ifeq ($(TARGET_BOARD_AUTO), true)
   ENABLE_SST_INVOKE_TEST := false
 endif #TARGET_BOARD_AUTO
 
+# Enable Hibernate kernel module
+ifeq ($(filter $(TARGET_BOARD_PLATFORM), seraph vienna64),$(TARGET_BOARD_PLATFORM))
+  ENABLE_HIBERNATE_TZDATA_MGR := true
+endif
+
 ifeq ($(ENABLE_QCRYPTO_DLKM), true)
 PRODUCT_PACKAGES += qcedev-mod_dlkm.ko
 PRODUCT_PACKAGES += qce50_dlkm.ko
@@ -167,3 +172,7 @@ endif #ENABLE_SI_CORE_TEST
 ifeq ($(ENABLE_SST_INVOKE_TEST), true)
 PRODUCT_PACKAGES_DEBUG += tornado_mod.ko
 endif #ENABLE_SST_INVOKE_TEST
+
+ifeq ($(ENABLE_HIBERNATE_TZDATA_MGR), true)
+PRODUCT_PACKAGES += hibernate_tzdata_mgr_dlkm.ko
+endif #ENABLE_HIBERNATE_TZDATA_MGR

securemsm_kernel_vendor_board.mk
@@ -80,6 +80,10 @@ ifeq ($(TARGET_BOARD_PLATFORM), gen5)
   endif
 endif
 
+# Enable Hibernate kernel module
+ifeq ($(filter $(TARGET_BOARD_PLATFORM), seraph vienna64),$(TARGET_BOARD_PLATFORM))
+  ENABLE_HIBERNATE_TZDATA_MGR_DLKM := true
+endif
 
 ifeq ($(ENABLE_QCRYPTO_DLKM), true)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/qcedev-mod_dlkm.ko \
@@ -131,3 +135,10 @@ endif #ENABLE_QSEECOM_DLKM
 ifeq ($(ENABLE_QCEDEV_FE), true)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/qcedev_fe_dlkm.ko
 endif #ENABLE_QCEDEV_FE
+
+ifeq ($(ENABLE_HIBERNATE_TZDATA_MGR_DLKM), true)
+BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/hibernate_tzdata_mgr_dlkm.ko
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/hibernate_tzdata_mgr_dlkm.ko
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(KERNEL_MODULES_OUT)/hibernate_tzdata_mgr_dlkm.ko
+endif #ENABLE_HIBERNATE_TZDATA_MGR_DLKM
+

CodeLinaro-mirror/le_platform_vendor_qcom_opensource_securemsm-kernel

securemsm_kernel_product_board.mk
@@ -115,6 +115,11 @@ ifeq ($(TARGET_BOARD_AUTO), true)
   ENABLE_SST_INVOKE_TEST := false
 endif #TARGET_BOARD_AUTO
 
+# Enable Hibernate kernel module
+ifeq ($(filter $(TARGET_BOARD_PLATFORM), seraph vienna),$(TARGET_BOARD_PLATFORM))
+  ENABLE_HIBERNATE_TZDATA_MGR := true
+endif
+
 ifeq ($(ENABLE_QCRYPTO_DLKM), true)
 PRODUCT_PACKAGES += qcedev-mod_dlkm.ko
 PRODUCT_PACKAGES += qce50_dlkm.ko
@@ -168,3 +173,7 @@ endif #ENABLE_SI_CORE_TEST
 ifeq ($(ENABLE_SST_INVOKE_TEST), true)
 PRODUCT_PACKAGES_DEBUG += tornado_mod.ko
 endif #ENABLE_SST_INVOKE_TEST
+
+ifeq ($(ENABLE_HIBERNATE_TZDATA_MGR), true)
+PRODUCT_PACKAGES += hibernate_tzdata_mgr_dlkm.ko
+endif #ENABLE_HIBERNATE_TZDATA_MGR

securemsm_kernel_vendor_board.mk
@@ -81,6 +81,10 @@ ifeq ($(TARGET_BOARD_PLATFORM), gen5)
   endif
 endif
 
+# Enable Hibernate kernel module
+ifeq ($(filter $(TARGET_BOARD_PLATFORM), seraph vienna),$(TARGET_BOARD_PLATFORM))
+  ENABLE_HIBERNATE_TZDATA_MGR_DLKM := true
+endif
 
 ifeq ($(ENABLE_QCRYPTO_DLKM), true)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/qcedev-mod_dlkm.ko \
@@ -136,3 +140,10 @@ endif #ENABLE_QSEECOM_DLKM
 ifeq ($(ENABLE_QCEDEV_FE), true)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/qcedev_fe_dlkm.ko
 endif #ENABLE_QCEDEV_FE
+
+ifeq ($(ENABLE_HIBERNATE_TZDATA_MGR_DLKM), true)
+BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/hibernate_tzdata_mgr_dlkm.ko
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/hibernate_tzdata_mgr_dlkm.ko
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(KERNEL_MODULES_OUT)/hibernate_tzdata_mgr_dlkm.ko
+endif #ENABLE_HIBERNATE_TZDATA_MGR_DLKM
+